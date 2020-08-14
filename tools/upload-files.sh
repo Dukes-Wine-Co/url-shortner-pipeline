76737594d4bbb47949e14a2aaede9471e9200e12
Tyrone Scafe <tscafejr@gmail.com>
2020-08-13 21:00:33 -0700

    Use bash for jenkinsfile

--- tools/upload-files.sh
+++ tools/upload-files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-. ./env.sh;
-. ./tools/configure-database.sh;
+source ./env.sh;
+source ./tools/configure-database.sh;
 
 (echo "Uploading files to mongodb" && uploadMongo) \
 && (echo "Uploading files to Google cloud storage" && uploadGcloud) \
